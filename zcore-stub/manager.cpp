7a7e8fc5cc8436c4115da5eb73b8a99bb21b3d12
Michael <m-garanin@yandex.ru>
2013-03-12 08:36:39 +0400

    ручной слияние по Issue 14

--- zcore-stub/manager.cpp
+++ zcore-stub/manager.cpp
@@ -66,7 +66,7 @@ void Manager::showLayerMax(int layer_id)
 }
 
 
-
+/*
 void Manager::getLastImage(int compkey, char **ppbuf, int *pw, int *ph)
 {
     Q_UNUSED(compkey);
@@ -80,6 +80,34 @@ void Manager::getLastImage(int compkey, char **ppbuf, int *pw, int *ph)
     *ppbuf = (char*)malloc(img.byteCount());    
     memcpy(*ppbuf, img.bits(), img.byteCount() );
 }
+*/
+void Manager::getLastImage(int compkey, char **ppbuf, int *pw, int *ph)
+{
+    const int circleRadius = width / 8;
+    int newHeigth = compkey % 2 == 0 ? (int)width / 4.0 * 3.0 : (int)width / 16.0 * 9.0;
+
+    QImage img(width, newHeigth, QImage::Format_RGB888);
+    img.fill(Qt::red);
+    QPainter painter(&img);
+    QFont f = painter.font();
+    f.setPixelSize(width / 12);
+    painter.setFont(f);
+    QString text = QString("%1 - %2").arg(compkey).arg(QTime::currentTime().toString());
+    QFontMetrics fm = painter.fontMetrics();
+    QSize textSize = fm.size(Qt::TextSingleLine, text);
+    QPen p = painter.pen();
+    p.setWidth(2);
+    painter.setPen(p);
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing, true);
+    painter.drawText(img.rect(), Qt::AlignHCenter | Qt::AlignVCenter, text);
+    painter.drawEllipse(img.rect().topLeft() + QPoint(circleRadius, circleRadius), circleRadius, circleRadius);
+    painter.drawEllipse(img.rect().bottomRight() - QPoint(circleRadius, circleRadius), circleRadius, circleRadius);
+    *pw = img.width();
+    *ph = img.height();
+    *ppbuf = (char*)malloc(img.byteCount());
+    memcpy(*ppbuf, img.bits(), img.byteCount() );
+}
+
 
 int Manager::addScene()
 {
