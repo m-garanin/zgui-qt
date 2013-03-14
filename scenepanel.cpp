14c7734cc5a511598b1416d9c135a459e675156f
Michael <m.garanin@broadcastnow.ru>
2013-03-14 12:01:55 +0300

    change rePosition

--- scenepanel.cpp
+++ scenepanel.cpp
@@ -67,34 +67,51 @@ void CScenePanel::resizeEvent(QResizeEvent *event)
     rePosition();
 }
 
+
 void CScenePanel::rePosition()
 {
-    // делим родительскую ширину между сценой и сеткой слоёв
+    int cols, rows;
+    int lwsize = _listLayerWidgets.size();
+
+    // определяем кол-во столбцов и строк
+    // 0,1 - один столбец
+    // 2,3,4 - два столбца
+    // > 5   - три столбца
+    cols = 3; // default
+    if(lwsize < 5){
+        cols = lwsize < 2 ? 1:2;
+    }
+
+    rows =  lwsize / cols;
+    rows = rows == 0? 1: rows;
+    if(cols*rows < lwsize)
+        rows += 1;
+
+    // делим родительскую ширину между сценой и сеткой слоёв пополам.
     // и высоту сетки делим между слоями.
     int w,h, sw, sh, sx, sy, zw, zh;
     w = this->width();
     h = this->height();
 
-    sx = w/2;
-    sy = 0;
 
     // параметры сетки
     zh = 7; // высота зазора между строками
     zw = 7; // ширина зазора между столбцами
-    sw = (w/2 - 2*zw)/3; // три столбца и два зазора
-    sh = (h - 2*zh)/3; // три ряда, высотой sh с двумя зазорами
+    sw = (w/2 - (cols-1)*zw)/cols; // ширина ячейки с учётом зазоров
+    sh = (h - (rows-1)*zh)/rows; //  высота ячейки с учётом зазоров
+
+    sx = w/2;
+    sy = 0;
 
     _sceneWidget->setGeometry(0, 0, w/2, h);
 
     for(int i=0; i<_listLayerWidgets.size(); i++){
-        if( i>0 && i % 3 == 0 ){
+        if( i>0 && i % cols == 0 ){
             sy += sh + zh;
             sx = w/2;
         }
-        // вставка зазора между столбцами
-        if(i % 3 != 0){
-            sx += zw;
-        }
+
+        sx += zw; // вставка зазора между столбцами
 
         _listLayerWidgets[i]->setGeometry(sx, sy, sw, sh);
         _listLayerWidgets[i]->show();
