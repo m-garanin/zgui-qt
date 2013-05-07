0a5a4fcd66e2d4b7d760ee9717f7ebcd71928564
Michael <m-garanin@yandex.ru>
2013-05-07 16:45:42 +0400

    add

--- main.cpp
+++ main.cpp
@@ -6,8 +6,17 @@ int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);    
-    QString t = "GST_PLUGIN_PATH=" + QCoreApplication::applicationDirPath();
-    putenv(t.toLocal8Bit().data());
+
+    #ifdef Q_OS_WIN32
+    QString t1 = "GST_PLUGIN_PATH=" + QCoreApplication::applicationDirPath();
+    _putenv(t1.toLocal8Bit().data());
+
+    char* gpath = getenv("GSTREAMER_SDK_ROOT_X86");
+    char* path = getenv("PATH");
+
+    QString t2 = "PATH=" + QString(path) + ";"+ QString(gpath) + "bin";
+    _putenv(t2.toLocal8Bit().data());
+    #endif
 
     MainWindow w;
     w.show();
