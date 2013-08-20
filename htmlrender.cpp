078903d84d01335b6fffa9586f03513c87b3a5bb
Michael <m-garanin@yandex.ru>
2013-08-20 22:39:44 +0300

    small clean

--- htmlrender.cpp
+++ htmlrender.cpp
@@ -12,7 +12,6 @@ HtmlRender::HtmlRender(QString name, QString fname, QObject *parent) :
     m_page = new QWebPage(this);
     m_page->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
     m_page->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-    //setSize(QSize(640, 480));
 
     QFile file(fname);
     file.open(QIODevice::ReadOnly | QIODevice::Text);
@@ -20,7 +19,7 @@ HtmlRender::HtmlRender(QString name, QString fname, QObject *parent) :
     m_page->mainFrame()->setHtml(file.readAll());
 
     connect(&m_timer, SIGNAL(timeout()), this, SLOT(updateFrame()));
-    m_timer.start(20);
+    m_timer.start(40);
 }
 
 void HtmlRender::setSize(const QSize &s)
@@ -33,27 +32,24 @@ void HtmlRender::setSize(const QSize &s)
 void HtmlRender::updateFrame()
 {    
     QSize size = m_page->mainFrame()->contentsSize();
-    //size.setHeight(size.width() * m_targetSize.height() / m_targetSize.width());
     QImage img = QImage(size, QImage::Format_ARGB32);
     img.fill(Qt::transparent);
 
-    qDebug() << size;
 
     QPalette pal = m_page->palette();
     pal.setBrush(QPalette::Base, Qt::transparent);
-    //view->page()->setPalette(pal);
     m_page->setPalette(pal);
 
 
     QPainter p(&img);
-    m_page->setViewportSize(m_page->mainFrame()->contentsSize());
+    m_page->setViewportSize(size);
+
     p.setRenderHint(QPainter::Antialiasing, true);
     p.setRenderHint(QPainter::TextAntialiasing, true);
     p.setRenderHint(QPainter::SmoothPixmapTransform, true);
+
     m_page->mainFrame()->render(&p);
     p.end();
-    //img = img.scaled(m_targetSize, Qt::KeepAspectRatioByExpanding);
-
     global_manager->sendExternalFrame(m_name.toLocal8Bit().data(), (char*)img.bits(), img.byteCount(), img.width(), img.height());
 
 }
